d6cff3bf6796a44950aab50613b0c75d978c4f2b
AlexJHartlaub <105608478+AlexJHartlaub@users.noreply.github.com>
2023-02-05 18:56:31 -0600

    Add files via upload

---
+++ Dice Roll/dice.js
@@ -0,0 +1,23 @@
+var dice = {
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * 6) + 1;
+      return randomNumber;
+    }
+  }
+  
+  
+  
+  //Prints dice roll to the page
+  
+  function printNumber(number) {
+    var placeholder = document.getElementById('number');
+    placeholder.innerHTML = number;
+  }
+  
+  var button = document.getElementById('button');
+  
+  button.onclick = function() {
+    var result = dice.roll();
+    printNumber(result);
+  };
+  
